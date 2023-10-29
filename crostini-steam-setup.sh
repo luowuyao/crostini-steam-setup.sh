bd8634a4aaf3a7ecbeafd0c24424fab051b86b77
邓文禧 <107822133+luowuyao@users.noreply.github.com>
2023-10-29 16:25:31 +0800

    Create crostini-steam-setup.sh

---
+++ crostini-steam-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -ex
+
+# Helper script to setup Steam testing environment.
+
+# Steam requires i386 architecture.
+echo "Adding i386 architecture"
+sudo dpkg --add-architecture i386
+# Add sources for Steam.
+
+# Reload after configuring apt configuration.
+echo "Updating APT"
+sudo apt update
+
+# TODO(davidriley): Remove this hack once a newer version of waffle has
+# been published.
+wget http://commondatastorage.googleapis.com/crosvm-apt-sandbox/waffle/libwaffle-1-0_1.6.0-4+b1_amd64.deb
+wget http://commondatastorage.googleapis.com/crosvm-apt-sandbox/waffle/libwaffle-1-0_1.6.0-4+b1_i386.deb
+sudo apt install -y ./libwaffle*.deb
+
+# Install packages.
+echo "Installing glxinfo, glxgears, steam, and apitrace"
+sudo apt install -y zstd mesa-utils steam apitrace apitrace-tracers:i386
